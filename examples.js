421d3411a385686096bbc6546b19263b77f937f5
Godfrey Yeung <godfrey@placemeter.com>
2016-07-04 00:22:30 -0700

    working graph generation from triples

---
+++ examples.js
@@ -0,0 +1,128 @@
+/**
+	Difference Categories (diffCats):
+		0 - Value
+		1 - Structural
+		2 - Extetnsion
+**/
+
+var examplesNT = [
+	{
+		"name": "coordinate location",
+		"diffCats": [1,2,3],
+		"databases": [
+			{
+				"dbName": "WikiData",
+				"triples": [
+					"https://www.wikidata.org/wiki/Q667443 https://www.wikidata.org/wiki/Property:P625 45°12'13''N,123°8'41''W",
+					"https://www.wikidata.org/wiki/Property:P625 https://www.wikidata.org/wiki/Property:P31 https://www.wikidata.org/wiki/Q18615777",
+					"https://www.wikidata.org/wiki/Property:P625 https://www.wikidata.org/wiki/Property:P1629 https://www.wikidata.org/wiki/Q22664",
+					"https://www.wikidata.org/wiki/Property:P625 https://www.wikidata.org/wiki/Property:P2667 https://www.wikidata.org/wiki/Q6294369"
+				]
+			},
+			{
+				"dbName": "DBPedia (FactForge)",
+				"triples": [
+					"http://dbpedia.org/resource/Hughes_H-4_Hercules http://www.georss.org/georss/point 45.204_-123.145",
+				],
+			}
+		]
+	},
+	{
+		"name": "vehicle type",
+		"diffCats": [1],
+		"databases": [
+			{
+				"dbName": "WikiData",
+				"triples": [
+					"https://www.wikidata.org/wiki/Property:P31 https://www.wikidata.org/wiki/Q1153376",
+					"",
+				]
+			}
+		]
+	}
+]
+
+var labels = {
+	"https://www.wikidata.org/wiki/Property:P31": "instance of",
+	"https://www.wikidata.org/wiki/Property:P1629": "subject item of this property",
+	"https://www.wikidata.org/wiki/Q667443": "Hughes H-4 Hercules",
+	"https://www.wikidata.org/wiki/Property:P625": "coordinate location",
+	"https://www.wikidata.org/wiki/Q22664": "geographic coordinate system",
+	"https://www.wikidata.org/wiki/Q18615777": "Wikidata property to indicate a location",
+	"https://www.wikidata.org/wiki/Property:P2667": "corresponding template",
+	"https://www.wikidata.org/wiki/Q6294369": "Template: Coord",
+	"45°12'13''N,123°8'41''W": "45°12'13''N, 123°8'41''W",
+	"https://www.wikidata.org/wiki/Q1153376":"flying boat"
+}
+
+var graph1 = {
+    "nodes":[
+      {"name":"a","width":60,"height":40},
+      {"name":"b","width":60,"height":40},
+      {"name":"c","width":60,"height":40},
+      {"name":"d","width":60,"height":40},
+      {"name":"e","width":60,"height":40},
+      {"name":"f","width":60,"height":40}
+    ],
+    "links":[
+      {"source":0,"target":1},
+      {"source":1,"target":2},
+      {"source":1,"target":3},
+      {"source":1,"target":4},
+      {"source":5,"target":4}
+    ],
+	"constraints":[
+	   {"type":"alignment",
+	   "axis":"y",
+	   "offsets":[
+	     {"node":"0", "offset":"0"},
+	     {"node":"1", "offset":"0"},
+	     {"node":"2", "offset":"0"}
+	   ]},
+	  {"type":"alignment",
+	   "axis":"y",
+	   "offsets":[
+	     {"node":"3", "offset":"0"},
+	     {"node":"4", "offset":"0"}
+	   ]},
+	   {"type":"alignment",
+	   "axis":"y",
+	   "offsets":[
+	     {"node":"5", "offset":"0"}
+	   ]}
+	]
+}
+
+var graph2 = {
+    "nodes":[
+      {"name":"a","width":60,"height":40},
+      {"name":"b","width":60,"height":40},
+      {"name":"c","width":60,"height":40},
+      {"name":"d","width":60,"height":40},
+      {"name":"e","width":60,"height":40},
+      {"name":"f","width":60,"height":40}
+    ],
+    "links":[
+      {"source":0,"target":4},
+      {"source":2,"target":4},
+      {"source":1,"target":4},
+      {"source":3,"target":4},
+      {"source":5,"target":4}
+    ],
+	"constraints":[
+	   {"type":"alignment",
+	   "axis":"y",
+	   "offsets":[
+	     {"node":"0", "offset":"0"},
+	     {"node":"1", "offset":"0"},
+	     {"node":"2", "offset":"0"}
+	   ]},
+	   {"type":"alignment",
+	   "axis":"y",
+	   "offsets":[
+	     {"node":"3", "offset":"0"},
+	     {"node":"4", "offset":"0"},
+	     {"node":"5", "offset":"0"}
+	   ]}
+	]
+}
---
+++ examplesAPI.js
@@ -0,0 +1,115 @@
+// examplesAPI
+
+function getLastToken(uri, delimiter){
+	var lastToken;
+	var tokenizedUri = uri.split("/");
+	lastToken = tokenizedUri[tokenizedUri.length-1];
+	return lastToken;
+}
+
+// acquire appropriate label
+function getLabel(uri, labels){
+	var label = "";
+	//check if corresponding label is on shelf
+	if(labels[uri])
+		label = labels[uri];
+	//else acquire characters after the last fwd slash in uri
+	else
+		label = getLastToken(uri,"/");
+		return label;
+	return
+}
+
+function addNode(uri, posInTriple, graphNodes, labels, graphConstraints, level){
+	node = {};
+	var added = 0;
+	var isPredicate = (posInTriple == 1) ? 1 : 0;
+	graphNodes.forEach(function(curVal, idx, arr){
+		if (curVal.uri == uri){
+			added = 1;
+			node = curVal;
+		}
+	});
+	if(!added){
+		label = getLabel(uri, labels);
+		//apparently cola.js lib requires width and height properties
+		//So I can't make my code cleaner elsewhere by just deriving them from isPred
+		if(isPredicate){
+			node = {"name": label, "uri":uri, "level": level, "isPred": 1, "width": 120, "height": 50, "x": (posInTriple-1)*100};
+		} else {
+			node = {"name": label, "uri":uri, "level": level, "isPred": 0, "width": 90, "height": 70, "x": (posInTriple-1)*100};
+		}
+		var nodeIdx = graphNodes.push(node) - 1;
+		while(!(graphConstraints.length - 1 >= level)){
+			graphConstraints.push({
+				"type":"alignment",
+				"axis":"y",
+				"offsets":[]
+			});
+		}
+		graphConstraints[level].offsets.push({"node":nodeIdx.toString(), "offset":"0"});
+	}
+	return node;
+}
+
+function getNode(uri, graphNodes){
+	var node = {};
+	graphNodes.forEach(function(curVal, idx, arr){
+		if(uri == curVal.uri)
+			node = curVal;
+	});
+	return node;
+}
+
+function addLinkByUris(sourceUri, targetUri, graphLinks, graphNodes){
+	var added = 0;
+	graphLinks.forEach(function(curVal, idx, arr){
+		if ( (curVal.sourceUri == sourceUri) && (curVal.targetUri == targetUri)) {
+			added = 1;
+		}
+	});
+	if(!added){
+		var source = getNode(sourceUri, graphNodes);
+		var target = getNode(targetUri, graphNodes);
+		graphLinks.push({"source": source, "target": target, "sourceUri": sourceUri, "targetUri": targetUri});
+	}
+}
+
+function addLinkByNodes(sourceNode, targetNode, graphLinks, graphNodes){
+	var added = 0;
+	graphLinks.forEach(function(curVal, idx, arr){
+		if ( (curVal.source == sourceNode) && (curVal.target == targetNode)) {
+			added = 1;
+		}
+	});
+	if(!added){
+		graphLinks.push({"source": sourceNode, "target": targetNode, "sourceUri": sourceUri, "targetUri": targetUri});
+	}
+}
+
+function dbExampleToGraph(dbExample, labels){
+	// creates new object each call, since the function's stack is destroyed upon return.
+	// However, each created graph obj is maintained on stack. Thus references towards these objects remain.
+	var graph = {};
+	graph.nodes = [];
+	graph.links = [];
+	graph.constraints = [];
+	dbExample.triples.forEach(function(curVal, idx, arr){
+		triple = curVal;
+		entities = triple.split(" ");
+		if(idx == 0){
+			addNode(entities[0], 0, graph.nodes, labels, graph.constraints, 0);
+			addNode(entities[1], 1, graph.nodes, labels, graph.constraints, 0);
+			addNode(entities[2], 2, graph.nodes, labels, graph.constraints, 0);
+		} else {
+			subjectNode = getNode(entities[0], graph.nodes); // assume subjects are always already added
+			predicateNode = addNode(entities[1], 1, graph.nodes, labels, graph.constraints, subjectNode.level + 1);
+			objectNode = addNode(entities[2], 2, graph.nodes, labels, graph.constraints, predicateNode.level + 1); // really just subjectNode+2, but this way is more flexbile
+		}
+
+		addLinkByUris(entities[0], entities[1], graph.links, graph.nodes);
+		addLinkByUris(entities[1], entities[2], graph.links, graph.nodes);
+	});
+
+	return graph;
+}
